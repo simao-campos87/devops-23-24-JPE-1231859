9a23136abea77799d779f52b8a1e7e699642d018
simao-campos87 <1231859@isep.ipp.pt>
2024-04-08 11:50:50 +0100

    #13 [new] added gradle spring boot gradle

---
+++ CA2/part2/react-and-spring-data-rest-basic/src/main/java/com/greglturnquist/payroll/ReactAndSpringDataRestBasicApplication.java
@@ -0,0 +1,13 @@
+package com.greglturnquist.payroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactAndSpringDataRestBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactAndSpringDataRestBasicApplication.class, args);
+	}
+
+}
---
+++ CA2/part2/react-and-spring-data-rest-basic/src/test/java/com/greglturnquist/payroll/ReactAndSpringDataRestBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greglturnquist.payroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactAndSpringDataRestBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
